cf9c5b89cb156ea13b238b4921f308549edf8eb2
Kim Beomhee <nakaldi@gmail.com>
2025-05-05 17:24:19 +0900

    Add 멤버 테이블 엔티티 추가

---
+++ src/main/java/beom/spring_practice/domain/Member.java
@@ -0,0 +1,103 @@
+package beom.spring_practice.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;  // 반드시 암호화 적용!
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Column(updatable = false, insertable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false, insertable = false)
+    private LocalDateTime updatedAt;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/beom/spring_practice/domain/Role.java
@@ -0,0 +1,5 @@
+package beom.spring_practice.domain;
+
+public enum Role {
+    USER, ADMIN
+}

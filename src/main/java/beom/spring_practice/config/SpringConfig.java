1bc6807687645de47256418d6259434859b3bed3
Kim Beomhee <nakaldi@gmail.com>
2025-05-05 18:02:43 +0900

    Add Config 클래스에 서비스, 리포지토리 빈 등록 코드 추가

--- src/main/java/beom/spring_practice/config/SpringConfig.java
+++ src/main/java/beom/spring_practice/config/SpringConfig.java
@@ -1,6 +1,10 @@
 package beom.spring_practice.config;
 
+import beom.spring_practice.repository.JpaMemberRepository;
+import beom.spring_practice.repository.MemberRepository;
+import beom.spring_practice.service.MemberService;
 import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -14,5 +18,15 @@ public class SpringConfig {
     public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
         this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
     }
 }

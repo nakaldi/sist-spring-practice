36c9b0f6a62b2ed4690450fb956c11ec638c5c39
Kim Beomhee <nakaldi@gmail.com>
2025-05-05 16:00:37 +0900

    Add mysql 연결 설정

--- src/main/java/beom/spring_practice/config/SpringConfig.java
+++ src/main/java/beom/spring_practice/config/SpringConfig.java
@@ -1,4 +1,18 @@
 package beom.spring_practice.config;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
 }
